a3abe9a5f1120bf5d1929bef2d636ba476845bb9
lynwo <skylyn@cs.wisc.edu>
2018-11-10 14:39:45 -0600

    Set up town buttons

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,11 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour {
 	public int townLocation = 1; // Make sure the town is build location 1
+	public int creditsLocation = 4;
 
 	public void LoadTown () 
 	{
-		
 		SceneManager.LoadScene(townLocation);
+	}
+
+	public void ShowSettings () 
+	{
+		// TODO: Preserve collier's Position and Velockitty
+
+
+	}
+
+	public void LoadCredits ()
+	{
+		SceneManager.LoadScene(creditsLocation);
+	}
+
+	public void ExitGame () 
+	{
+		Application.Quit();
 	}
 
 }
