f56f15ae6ba31dd87acf5a92fa7339b1ebf67134
lynwo <skylyn@cs.wisc.edu>
2018-11-10 13:02:38 -0600

    Connects title to town

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+	public int townLocation = 1; // Make sure the town is build location 1
+
+	public void LoadTown () 
+	{
+		
+		SceneManager.LoadScene(townLocation);
+	}
+
+}
+
