cf358a40c24866032b2ab80a69e3010cd16dc882
Evans Chen <evansschen@gmail.com>
2018-11-11 01:38:36 -0600

    Can now fo up through platforms
    hoorah

---
+++ Collier/Assets/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    BoxCollider2D box;
+
+	// Use this for initialization
+	void Start () {
+        box = GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        box.enabled = transform.position.y
+            < GameObject.FindGameObjectWithTag("Player").transform.position.y;
+    }
+}
