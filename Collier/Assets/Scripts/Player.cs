944a2d4299945cbd8c2ceb820ae0d0fda7647dc0
Evans Chen <evansschen@gmail.com>
2019-03-30 13:32:47 -0500

    Adds the level select scenes
    38 changed files huh

--- Collier/Assets/Scripts/Player.cs
+++ Collier/Assets/Scripts/Player.cs
@@ -369,13 +369,23 @@ public class Player : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.collider.gameObject.tag == "Goal" && SceneManager.GetActiveScene().name != "1_Town")
+        if (col.collider.gameObject.tag == "Goal" && InLevel())
         {
             if (!damaged && !dead)
             {
                 win = true;
             }
         }
+    }
+
+    public static bool InLevel()
+    {
+        return SceneManager.GetActiveScene().name != "1_Town"
+                && SceneManager.GetActiveScene().name != "Level_Select_1"
+                && SceneManager.GetActiveScene().name != "Level_Select_2"
+                && SceneManager.GetActiveScene().name != "Level_Select_3"
+                && SceneManager.GetActiveScene().name != "Level_Select_4"
+                && SceneManager.GetActiveScene().name != "Level_Select_5";
     }
 
     bool Grounded()
---
+++ Collier/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SaveLoad : MonoBehaviour
+{
+    static bool loaded = false;
+    static Dictionary<string, int> levelUnlocked = new Dictionary<string, int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!loaded)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    string key = $"Level_{i}_{j}";
+                    if (!PlayerPrefs.HasKey(key))
+                    {
+                        PlayerPrefs.SetInt(key, -1);
+                    }
+                    levelUnlocked[key] = PlayerPrefs.GetInt(key);
+                }
+            }
+            loaded = true;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    string key = $"Level_{i}_{j}";
+                    PlayerPrefs.DeleteKey(key);
+                }
+            }
+            loaded = false;
+        }
+    }
+}
