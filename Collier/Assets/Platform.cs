ea5de87654cb90b3bd09ff48a8d0606723942fd2
Savannah Mann <mannnnnn@users.noreply.github.com>
2019-08-29 18:45:58 -0500

    Wiring in new art

--- Collier/Assets/Platform.cs
+++ Collier/Assets/Platform.cs
@@ -8,15 +8,17 @@ public class Platform : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        box = GetComponent<BoxCollider2D>();
+         /*box = GetComponent<BoxCollider2D>();*/
     }
 	
 	// Update is called once per frame
 	void Update () {
+        /*I've got a bad feeling about this code
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
             box.enabled = transform.position.y < player.transform.position.y;
         }
+         */
     }
 }
